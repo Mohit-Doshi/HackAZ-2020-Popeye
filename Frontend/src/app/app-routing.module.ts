933bda9fd3f8e21958db1433fdaa66eeb61e20a1
yash101 <lodhad@gmail.com>
2020-01-18 23:20:05 -0700

    Removing unused components to refresh with newer ones

--- Frontend/src/app/app-routing.module.ts
+++ Frontend/src/app/app-routing.module.ts
@@ -1,18 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import { OverviewComponent } from './overview/overview.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { GitHubComponent } from './git-hub/git-hub.component';
-import { ResumeComponent } from './resume/resume.component';
-import { TimelineComponent } from './timeline/timeline.component';
 
 
 const routes: Routes = [
-  { path: 'overview', component: OverviewComponent },
-  { path: 'projects', component: ProjectsComponent },
-  { path: 'GitHub', component: GitHubComponent },
-  { path: 'resume', component: ResumeComponent },
-  { path: 'timeline', component: TimelineComponent },
+  // { path: 'overview', component: OverviewComponent },
+  // { path: 'projects', component: ProjectsComponent },
+  // { path: 'GitHub', component: GitHubComponent },
+  // { path: 'resume', component: ResumeComponent },
+  // { path: 'timeline', component: TimelineComponent },
 ];
 
 @NgModule({
--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -5,12 +5,6 @@ import { MatTabsModule } from '@angular/material';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ContentComponent } from './content/content.component';
-import { OverviewComponent } from './overview/overview.component';
-import { ProjectsComponent } from './projects/projects.component';
-import { GitHubComponent } from './git-hub/git-hub.component';
-import { ResumeComponent } from './resume/resume.component';
-import { TimelineComponent } from './timeline/timeline.component';
 import { NavigationComponent } from './navigation/navigation.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule } from '@angular/material/toolbar';
@@ -22,12 +16,6 @@ import { MatListModule } from '@angular/material/list';
 @NgModule({
   declarations: [
     AppComponent,
-    ContentComponent,
-    OverviewComponent,
-    ProjectsComponent,
-    GitHubComponent,
-    ResumeComponent,
-    TimelineComponent,
     NavigationComponent
   ],
   imports: [
